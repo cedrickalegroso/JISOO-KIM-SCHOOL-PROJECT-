c2999ec38e7dd61ef0043bd1cd75a45fe8850247
Dancedrick <personal.dancedrick@gmail.com>
2021-11-07 20:17:57 +0800

    TADAA!

---
+++ script-dist.js
@@ -0,0 +1,2 @@
+gsap.fromTo(".contentinner1",{y:2e3,opacity:0},{y:0,duration:1,opacity:1}),gsap.fromTo(".contentinner2",{y:2e3,opacity:0},{y:0,duration:1,opacity:1});
+//# sourceMappingURL=script-dist.js.map
---
+++ script.js
@@ -0,0 +1,8 @@
+//alert("Not made to be responsive.")\
+
+
+
+
+gsap.fromTo(".contentinner1", {y: 2000, opacity: 0}, {y: 0, duration: 1, opacity: 1});
+
+gsap.fromTo(".contentinner2", {y: 2000, opacity: 0}, {y: 0, duration: 1, opacity: 1});
